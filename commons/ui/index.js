27bb9a2abcafcdaa475d34016ca102957b0f01db
fajar <nugrahafajar37@gmail.com>
2025-10-30 00:39:50 +0700

    feat: add logo.svg, update main.css to integrate Tailwind CSS, and refactor index.js; remove index.ts and clean up package.json

---
+++ commons/ui/index.js
@@ -0,0 +1,7 @@
+
+import "./style/reset.css";
+import "./style/normalize.css";
+import "./style/variables.css";
+
+import "./assets/base.css";
+import "./assets/main.css";
--- commons/ui/index.ts
+++
@@ -1,4 +0,0 @@
-export function ui(): void {
-  console.log("this is ui components");
-}
-
